d5da803456c15652894dc2192950dfefcc9039e4
Allan Garcia <allan@apli.jobs>
2020-08-04 16:21:32 -0500

    Restructured and added comments.

--- structures/structures.go
+++ structures/structures.go
@@ -1,39 +1,41 @@
+/*
+Package structures provides implementations of basic data structures.
+- Stack: First in, last out
+*/
 package structures
 
 import "fmt"
 
-// Type definitions
-type dataStructure struct {
-	size   uint
-	last   int
+// DataStructure is the base type for structures that will need to implement
+// how the data is organized and accesed.
+type DataStructure struct {
 	values []float64
 }
 
-type accesor interface {
-	Insert(float64)
+// Interface type for all data structures that will implement retrieval
+// methods for data access.
+type dataAccesor interface {
 	Pop() float64
+	Peek() float64
 	PrintInfo()
 }
 
-// Stack ...
+// Stack is the data structure that provides a last in, first out
+// (LIFO) data organization.
 type Stack struct {
-	dataStructure
+	DataStructure
 }
 
-// General methods
-func (ds *dataStructure) Init(size uint) {
-	// Initialize structure fields
-	ds.size = size
-	ds.last = -1
-
-	// Initialize the array
-	ds.values = make([]float64, 0, size)
+// Init pre-allocates the memory used for the data structure
+func (ds *DataStructure) Init(capacity uint) {
+	ds.values = make([]float64, 0, capacity)
 }
 
-func (ds dataStructure) PrintValues() {
+// PrintValues ...
+func (ds DataStructure) PrintValues() {
 	var comma string
 	fmt.Print("[")
-	for i := 0; i <= ds.last; i++ {
+	for i := 0; i < len(ds.values); i++ {
 		if i != 0 {
 			comma = ", "
 		} else {
@@ -44,10 +46,21 @@ func (ds dataStructure) PrintValues() {
 	fmt.Print("]\n")
 }
 
-func (ds dataStructure) PrintInfo() {
+// PrintInfo displays information about the data structure.
+func (ds *DataStructure) PrintInfo() {
 	fmt.Printf("Length: %v\n", len(ds.values))
 	fmt.Printf("Capacity: %v\n", cap(ds.values))
 	fmt.Printf("Values: %v\n", ds.values)
+}
+
+// Insert adds a value into the structure.
+func (ds *DataStructure) Insert(value float64) {
+	ds.values = append(ds.values, value)
+}
+
+// GetSize returns the amount of values appended into the structure.
+func (ds *DataStructure) GetSize() int {
+	return int(len(ds.values))
 }
 
 /*
@@ -60,16 +73,10 @@ func (ds dataStructure) PrintInfo() {
  * Stack methods
  */
 
-// Insert ...
-func (s *Stack) Insert(value float64) {
-	s.last++
-	s.values = append(s.values, value)
-}
-
-// Pop ...
+// Pop returns and removes the last value introduced into stack.
 func (s *Stack) Pop() float64 {
-	var value = s.values[s.last]
-	s.values = s.values[:s.last]
-	s.last--
+	var last int = len(s.values) - 1
+	var value float64 = s.values[last]
+	s.values = s.values[:last]
 	return value
 }
